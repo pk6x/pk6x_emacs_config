bebbc646e9e671e5791ddcb55fb65d337cefb49a
pk6x <a.mgundam78@gmail.com>
2024-11-27 20:43:45 +0000

    Updated

--- misc/win32_emacs_devenv/build_common_example.bat
+++ misc/win32_emacs_devenv/build_common_example.bat
@@ -2,7 +2,7 @@
 
 IF NOT EXIST ..\build mkdir ..\build
 pushd ..\build
-cl -Oi -W4 -WX -wd4201 -wd4100 -wd4189 -FC -Z7 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 ..\..\me_handmadehero\code\win32_handmade.cpp /link -subsystem:windows user32.lib gdi32.lib
+cl -Oi -W4 -WX -wd4201 -wd4100 -wd4189 -FC -Z7 -Fm -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 ..\..\me_handmadehero\code\win32_handmade.cpp /link -subsystem:windows user32.lib gdi32.lib
 popd
 
 REM Compiler options:
@@ -13,7 +13,8 @@ REM -wx, is to treat warnings as errors thus failure in compiling unless warning
 REM -wdxxxx, is to ignore a specific warning
 REM -FC, is to display the full path of source code files passed to cl.exe in diagnostic text
 REM -Z7, is to generate complete debugging information/files except (vc10.pdb) 
-REM -D<name>, is to define constants and macros  
+REM -D<name>, is to define constants and macros
+REM -Fm, is to generate a map file 
 
 REM Link options:
 REM /link, is the call for the linker
