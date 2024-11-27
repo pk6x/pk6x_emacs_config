be5225e73f5be06a930376bfb7faf862b1c93dc9
pk6x <a.mgundam78@gmail.com>
2024-11-27 22:54:09 +0000

    Updated

--- misc/win32_emacs_devenv/build_common_example.bat
+++ misc/win32_emacs_devenv/build_common_example.bat
@@ -2,12 +2,13 @@
 
 IF NOT EXIST ..\build mkdir ..\build
 pushd ..\build
-cl -Oi -W4 -WX -wd4201 -wd4100 -wd4189 -FC -Z7 -Fm -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 ..\..\me_handmadehero\code\win32_handmade.cpp /link -subsystem:windows user32.lib gdi32.lib
+cl -Oi -Od -W4 -WX -wd4201 -wd4100 -wd4189 -FC -Z7 -Fm -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 ..\..\me_handmadehero\code\win32_handmade.cpp /link -subsystem:windows user32.lib gdi32.lib
 popd
 
 REM Compiler options:
 REM cl, is cl.exe, is the call for MSVC compiler on Windows
-REM -Oi, is for intrinsic optimization
+REM -Oi, is to generate intrinsic functions
+REM -Od, is to disable optimization 
 REM -w4, is a warning level 4
 REM -wx, is to treat warnings as errors thus failure in compiling unless warning got resolved
 REM -wdxxxx, is to ignore a specific warning
